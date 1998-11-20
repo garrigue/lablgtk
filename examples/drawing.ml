b0713d9f0172a997d0d4003bd953e55485e8cc91
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1998-11-20 10:07:01 +0000

    work on gdk
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@28 9e25d42d-7a03-43f2-900d-8c2168964d28

---
+++ examples/drawing.ml
@@ -0,0 +1,20 @@
+(* $Id$ *)
+
+open Gtk
+open Gdk
+
+(* let id = Thread.create GtkThread.main () *)
+let top = Window.create `TOPLEVEL
+let _ = Widget.show top
+let w = Widget.window top
+let gc = GC.create w
+
+let _ =
+  Signal.connect top sig:Signal.expose_event cb:
+    begin fun _ ->
+      Draw.polygon w gc filled:true
+	[ 10,100; 35,35; 100,10; 165,35; 190,100;
+	  165,165; 100,190; 35,165; 10,100 ]
+    end;
+  (* Thread.join id *)
+  Main.main ()
