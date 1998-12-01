5b52d033fcf403a8ec24d0300e6d63ba54d5d3ae
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1998-12-01 01:35:38 +0000

    cleanup

--- src/ml_gtk.h
+++ src/ml_gtk.h
@@ -3,4 +3,8 @@
 #define GtkObject_val(obj) ((GtkObject*)Field(obj,1))
 extern value Val_GtkObject (GtkObject *w);
 
+#ifdef GTK_NO_CHECK_CASTS
+#define check_cast(f,v) f(v)
+#else
 #define check_cast(f,v) (Pointer_val(v) == NULL ? NULL : f(Pointer_val(v)))
+#endif
