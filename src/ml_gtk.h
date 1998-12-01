87cbe67520c93c9be80356a0f29c9c23fd702f81
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1998-12-01 04:44:42 +0000

    bug

--- src/ml_gtk.h
+++ src/ml_gtk.h
@@ -4,7 +4,7 @@
 extern value Val_GtkObject (GtkObject *w);
 
 #ifdef GTK_NO_CHECK_CASTS
-#define check_cast(f,v) f(v)
+#define check_cast(f,v) f(Pointer_val(v))
 #else
 #define check_cast(f,v) (Pointer_val(v) == NULL ? NULL : f(Pointer_val(v)))
 #endif
