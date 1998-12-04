fef49b7ec8d6f6f76be20468f722be68c5aa259b
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1998-12-04 00:50:45 +0000

    termination rules
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@86 9e25d42d-7a03-43f2-900d-8c2168964d28

--- examples/pousse.ml
+++ examples/pousse.ml
@@ -4,7 +4,11 @@ open Gtk
 open GtkObj
 open GdkObj
 
+(* Toplevel window *)
+
 let window = new_window `TOPLEVEL
+
+(* Create pixmaps *)
 
 let pixdraw = new pixdraw parent:window width:40 height:40
 let pixdraw1 = new pixdraw parent:window width:40 height:40
@@ -17,6 +21,8 @@ let _ =
   pixdraw2#arc x:3 y:3 width:34 height:34 filled:true;
   pixdraw2#set foreground:`Black;
   pixdraw2#arc x:3 y:3 width:34 height:34
+
+(* The cell class: a button with a pixmap on it *)
 
 class cell () = object (self)
   inherit button (Button.create ())
@@ -34,9 +40,13 @@ class cell () = object (self)
     ()
 end
 
+(* Our board: a matrix of cells *)
+
 let cells =
   Array.init len:8
     fun:(fun _ -> Array.init len:8 fun:(fun _ -> new cell ()))
+
+(* The game logic *)
 
 let on_board x y =
   x >= 0 && x < 8 && y >= 0 && y < 8
@@ -45,23 +55,32 @@ let rec string :x :y :dx :dy :color l =
   let x = x+dx and y = y+dy in
   if on_board x y then
     let col = cells.(x).(y)#color in 
-    if col = color then l else
+    if col = (color : [white black] :> [white black none]) then l else
     if col = `none then [] else
     string :x :y :dx :dy :color ((x,y)::l)
   else []
 
+let find_swaps x y :color =
+  if cells.(x).(y)#color <> `none then [] else
+  List.fold_left [-1,-1; -1,0; -1,1; 0,-1; 0,1; 1,-1; 1,0; 1,1]
+    acc:[]
+    fun:(fun :acc (dx,dy) -> string :x :y :dx :dy :color [] @ acc)
+
 let action x y :color =
-  let color = (color : [white black] :> [white black none]) in
-  if cells.(x).(y)#color <> `none then false else
-  let swaps =
-    List.fold_left [-1,-1; -1,0; -1,1; 0,-1; 0,1; 1,-1; 1,0; 1,1]
-      acc:[]
-      fun:(fun :acc (dx,dy) -> string :x :y :dx :dy :color [] @ acc)
-  in
+  let swaps = find_swaps x y :color in
   if swaps = [] then false else begin
-    List.iter ((x,y)::swaps) fun:(fun (x,y) -> cells.(x).(y)#set_color color);
+    List.iter ((x,y)::swaps)
+      fun:(fun (x,y) -> cells.(x).(y)#set_color (color :> [white black none]));
     true
   end
+
+let check_impossible color =
+  try
+    for i = 0 to 7 do for j = 0 to 7 do
+      if find_swaps i j :color <> [] then raise Exit
+    done done;
+    true
+  with Exit -> false
 
 let count_cells () =
   Array.fold_left
@@ -72,8 +91,11 @@ let count_cells () =
        end)
     (0,0) cells
 
+(* Graphical elements *)
+
 let table = new_table columns:8 rows:8
 let vbox = new_box `VERTICAL
+let frame' = new_frame ()
 let hbox = new_box `HORIZONTAL
 let frame = new_frame ()
 let label = new_label label:""
@@ -86,13 +108,55 @@ let bar = new_statusbar ()
 let turn = bar#new_context name:"turn"
 let messages = bar#new_context name:"messages"
 
-let current_color = ref `white
+(* Conducting a game *)
+
+class game = object (self)
+  val mutable current_color = `black
+  method player = current_color
+  method swap_players () =
+    current_color <-
+      match current_color with
+	`white -> turn#pop (); turn#push "Player is black"; `black
+      | `black -> turn#pop (); turn#push "Player is white"; `white
+  method finish () =
+    turn#pop ();
+    let w, b = count_cells () in
+    turn#push
+      (if w > b then "White wins" else
+       if w < b then "Black wins" else
+       "Game is a draw");
+    ()
+  method play x y =
+    if action x y color:current_color then begin
+      update_label ();
+      self#swap_players ();
+      if check_impossible current_color then begin
+	self#swap_players ();
+	if check_impossible current_color then self#finish ()
+      end
+    end else
+      messages#flash "You cannot play there"
+  initializer
+    cells.(3).(3)#set_color `white;
+    cells.(4).(4)#set_color `white;
+    cells.(3).(4)#set_color `black;
+    cells.(4).(3)#set_color `black;
+    update_label ();
+    turn#push "Player is black";
+    ()
+end
+
+let game = new game
+
+(* Packing and connecting *)
 
 let _ =
   window#connect#destroy callback:Main.quit;
   window#set title:"pousse";
   window#add vbox;
-  vbox#add table;
+  vbox#add frame';
+  frame'#add table;
+  frame'#set shadow_type:`IN;
   vbox#add hbox;
   hbox#add bar;
   hbox#pack frame expand:false;
@@ -103,24 +167,8 @@ let _ =
     let cell = cells.(i).(j) in
     cell#connect#event#enter_notify
       callback:(fun _ -> cell#misc#grab_focus (); false);
-    cell#connect#clicked callback:
-      begin fun () ->
-	if action i j color:!current_color then begin
-	  update_label ();
-	  current_color :=
-	    match !current_color with
-	      `white -> turn#pop (); turn#push "Player is black"; `black
-	    | `black -> turn#pop (); turn#push "Player is white"; `white
-	end else
-	  messages#flash "You cannot play there"
-      end;
+    cell#connect#clicked callback:(fun () -> game#play i j);
     table#attach left:i top:j cell
   done done;
-  cells.(3).(3)#set_color `white;
-  cells.(4).(4)#set_color `white;
-  cells.(3).(4)#set_color `black;
-  cells.(4).(3)#set_color `black;
-  update_label ();
-  turn#push "Player is white";
   window#show_all ();
   Main.main ()
