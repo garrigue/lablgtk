8121139a18740f881c5a8ef2e6b7d708d1e24e78
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1998-12-07 05:57:40 +0000

    use inheritance
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@92 9e25d42d-7a03-43f2-900d-8c2168964d28

--- threadObj.ml
+++ threadObj.ml
@@ -1,24 +1,49 @@
 (* $Id$ *)
 
-class ['a] threaded dispatcher = object (self)
-  val dispatcher : 'a -> unit = dispatcher
+class virtual ['a] threaded () = object (self)
   val mutex = Mutex.create ()
-  method private dispatch msg =
-    Mutex.lock mutex;
-    dispatcher msg;
-    Mutex.unlock mutex
+  method private virtual dispatch : 'a -> unit
   method send msg =
-    Thread.create (self#dispatch) msg;
+    Thread.create
+      (fun msg ->
+	try
+	  Mutex.lock mutex;
+	  self#dispatch msg;
+	  Mutex.unlock mutex
+	with exn ->
+	  Mutex.unlock mutex; raise exn)
+      msg;
     ()
 end
 
-(*
-let proxy obj =
-  let dispatcher (msg : [a(int) b c(int * bool)]) =
-    match msg with
-      `a n -> obj#a n
-    | `b -> obj#b ()
-    | `c(n,b) -> obj#c n b
+let threader dispatcher =
+  let mutex = Mutex.create () in
+  let dispatch msg =
+    Mutex.lock mutex;
+    dispatcher msg;
+    Mutex.unlock mutex
   in
-  new threaded dispatcher
+  fun msg -> Thread.create dispatch msg; ()
+
+(*
+class thsafe_obj () = object (self)
+  inherit ['a] threaded ()
+  val super = new unsafe_obj ()
+  method private dispatch = 
+    function
+	`a -> super#a
+      | ...
+  method a = call `a
+      ....
+end
 *)
+
+class virtual ['a] server () = object (self)
+  inherit ['a] threaded ()
+  method bind =
+    Thread.create
+      (fun ic ->
+	let msg = input_value ic in
+	self#send msg)
+end
+
