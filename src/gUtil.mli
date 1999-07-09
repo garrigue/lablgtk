cd8944db5daed4cbb8eeb3cd47cfa14dc7f57276
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1999-07-09 08:33:13 +0000

    ML signals

--- src/gUtil.mli
+++ src/gUtil.mli
@@ -2,6 +2,12 @@
 
 open GObj
 
+(* The memo class provides an easy way to remember the real class of
+   a widget.
+   Insert all widgets of class in one single t memo, and you can then
+   recover their original ML object with #find.
+*)
+
 class ['a] memo : unit ->
   object
     constraint 'a = #gtkobj
@@ -9,4 +15,51 @@ class ['a] memo : unit ->
     method add : 'a -> unit
     method find : #gtkobj -> 'a
     method remove : #gtkobj -> unit
+  end
+
+(* To add ML signals to an object:
+
+   class mywidget_signals obj ?:after :signals =
+     let mysignal1, mysignal2 : t1 signal * t2 signal = signals in
+     object
+       inherit somewidget obj ?:after
+       method mysignal1 = mysignal1#connect ?:after
+       method mysignal2 = mysignal2#connect ?:after
+     end
+
+   class mywidget obj = object (self)
+     inherit somewidget obj
+     inherit has_ml_signals obj
+     val mysignal1 = new signal
+     val mysignal2 = new signal
+     method connect = new mywidget_signals ?obj
+     method call1 = mysignal1#call
+     method call2 = mysignal2#call
+     initializer
+       self#add_signal mysignal1;
+       self#add_signal mysignal2
+   end
+
+   Remark: you only need to inherit once from has_ml_signals.
+   If you want to add new signals do not inherit again.
+*)
+
+val next_callback_id : unit -> GtkSignal.id
+
+class ['a] signal :
+  object
+    method call : 'a -> unit
+    method connect : callback:('a -> unit) -> ?after:bool -> GtkSignal.id
+    method disconnect : GtkSignal.id -> bool
+    method reset : unit -> unit
+  end
+class type disconnector =
+  object
+    method disconnect : GtkSignal.id -> bool
+    method reset : unit -> unit
+  end
+class has_ml_signals : 'a Gtk.obj ->
+  object
+    method private add_signal : 'b signal -> unit
+    method disconnect : GtkSignal.id -> unit
   end
