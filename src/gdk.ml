1779a0c79bbe74e48792b810a31d254e743341f7
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
1999-07-14 03:28:37 +0000

    preparing release

--- src/gdk.ml
+++ src/gdk.ml
@@ -151,7 +151,7 @@ module Rectangle = struct
 end
 
 module Window = struct
-  type backgroundPixmap = [ NONE PARENT_RELATIVE PIXMAP(pixmap) ]
+  type background_pixmap = [ NONE PARENT_RELATIVE PIXMAP(pixmap) ]
   external visual_depth : visual -> int = "ml_gdk_visual_get_depth"
   external get_visual : window -> visual = "ml_gdk_window_get_visual"
   external get_parent : window -> window = "ml_gdk_window_get_parent"
@@ -167,7 +167,7 @@ module Window = struct
     let null_pixmap = (Obj.magic null : pixmap) in
     match pix with
       `NONE -> set_back_pixmap w null_pixmap 0
-    | `PARENT_RELATVIE -> set_back_pixmap w null_pixmap 1
+    | `PARENT_RELATIVE -> set_back_pixmap w null_pixmap 1
     | `PIXMAP(pixmap) -> set_back_pixmap w pixmap 0 
        (* anything OK, Maybe... *) 
 end
@@ -291,4 +291,60 @@ module DnD = struct
       = "ml_GdkDragContext_suggested_action"
   external drag_context_targets : drag_context -> atom list
       = "ml_GdkDragContext_targets"
+end
+
+module Truecolor = struct
+  (* Truecolor quick color query *) 
+
+  type visual_shift_prec = {
+      red_shift : int;
+      red_prec : int;
+      green_shift : int;
+      green_prec : int;
+      blue_shift : int;
+      blue_prec : int
+    }
+ 
+  let shift_prec visual = {
+    red_shift = Visual.red_shift visual;
+    red_prec = Visual.red_prec visual;
+    green_shift = Visual.green_shift visual;
+    green_prec = Visual.green_prec visual;
+    blue_shift = Visual.blue_shift visual;
+    blue_prec = Visual.blue_prec visual;
+  }
+
+  let color_creator visual =
+    match Visual.get_type visual with
+      `TRUE_COLOR | `DIRECT_COLOR ->
+	let shift_prec = shift_prec visual in
+	Format.eprintf "red : %d %d, "
+	  shift_prec.red_shift shift_prec.red_prec;
+	Format.eprintf "green : %d %d, "
+	  shift_prec.green_shift shift_prec.green_prec;
+	Format.eprintf "blue : %d %d"
+	  shift_prec.blue_shift shift_prec.blue_prec;
+	Format.pp_print_newline Format.err_formatter ();
+	let red_lsr = 16 - shift_prec.red_prec
+	and green_lsr = 16 - shift_prec.green_prec
+	and blue_lsr = 16 - shift_prec.blue_prec in
+	fun red: red green: green blue: blue ->
+	  (((red lsr red_lsr) lsl shift_prec.red_shift) lor 
+    	   ((green lsr green_lsr) lsl shift_prec.green_shift) lor
+    	   ((blue lsr blue_lsr) lsl shift_prec.blue_shift))
+    | _ -> raise (Invalid_argument "Gdk.Truecolor.color_creator")
+
+  let color_parser visual =
+    match Visual.get_type visual with
+      `TRUE_COLOR | `DIRECT_COLOR ->
+	let shift_prec = shift_prec visual in
+	let red_lsr = 16 - shift_prec.red_prec
+	and green_lsr = 16 - shift_prec.green_prec
+	and blue_lsr = 16 - shift_prec.blue_prec in
+	let mask = 1 lsl 16 - 1 in
+	fun pixel ->
+	  ((pixel lsr shift_prec.red_shift) lsl red_lsr) land mask,
+	  ((pixel lsr shift_prec.green_shift) lsl green_lsr) land mask,
+	  ((pixel lsr shift_prec.blue_shift) lsl blue_lsr) land mask
+    | _ -> raise (Invalid_argument "Gdk.Truecolor.color_parser")
 end
