9e92b761e33e7cabac99e70767b359443049e05a
furuse <furuse@9e25d42d-7a03-43f2-900d-8c2168964d28>
1999-07-15 05:52:54 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@222 9e25d42d-7a03-43f2-900d-8c2168964d28

--- applications/xxaplay/dsp.ml
+++ applications/xxaplay/dsp.ml
@@ -30,36 +30,52 @@ let _ =
   Callback.register_exception "dsp_error" (Error "");
   init ()
 
+let dspmay f dsp =
+  match dsp with
+    Some dsp -> f dsp
+  | None -> raise (Error "dsp device is not opened")
+
 class dspplay dev = 
-  let dsp_fd = open_device dev [O_WRONLY] 0 in
-  object
-  val dsp = dsp_fd
-  val block_size = get_block_size dsp_fd
+  object (self)
+  val mutable dsp = None 
+  val mutable block_size = 0
   val mutable buf = ""
   val mutable length = 0
 
-  method get_block_size = block_size
-  method set_sample_size = set_sample_size dsp
-  method set_stereo = set_stereo dsp
-  method set_speed = set_speed dsp
-  method sync = sync dsp
-  method close = close dsp
+  method open_dsp = 
+    match dsp with
+      Some dsp -> ()
+    | None ->
+	let d = open_device dev [O_WRONLY] 0 in
+	block_size <- get_block_size d;
+	dsp <- Some d
+
+  method get_block_size = dspmay (fun _ -> block_size) dsp
+
+  method set_sample_size = dspmay set_sample_size dsp
+  method set_stereo = dspmay set_stereo dsp
+  method set_speed = dspmay set_speed dsp
+  method sync = dspmay sync dsp
+  method close = Misc.may (fun d -> close d; dsp <- None) dsp
+
   method write s =
     let slength = String.length s in
     let newbuf = if buf = "" then s else buf ^ s in
     let all = length + slength in
     let remain = ref all in
     let at = ref 0 in
+    self#open_dsp;
     while !remain >= block_size do
-      write dsp newbuf !at block_size;
+      dspmay write dsp newbuf !at block_size;
       remain := !remain - block_size;
       at := !at + block_size
     done;
     buf <- String.sub newbuf (all - !remain) !remain;
     length <- !remain
   method flush =
-    write dsp buf 0 length;
-    buf <- "";
-    length <- 0
+    Misc.may (fun dsp -> 
+      write dsp buf 0 length;
+      buf <- "";
+      length <- 0) dsp
 end
 
