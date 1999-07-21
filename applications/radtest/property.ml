0597157a37df3d53b703061e269f47c94daa5e34
fauque <fauque@9e25d42d-7a03-43f2-900d-8c2168964d28>
1999-07-21 17:05:08 +0000

    new version of radtest

--- applications/radtest/property.ml
+++ applications/radtest/property.ml
@@ -7,29 +7,56 @@ open GMisc
 open GWindow
 
 
-class ['a] rval :init ?inits:inits [< "" >] :setfun ?:value_list [< [] >] = object
+class ['a] rval :init ?inits:inits [< "" >] :setfun ?:value_list [< [] >] = object(self)
   val mutable value : 'a = init
   val mutable value_string : string = inits
   val value_list : (string * 'a) list = value_list
   method value = value
   method value_list = value_list
   method value_string = value_string
-  method set : 'a -> n:string -> unit = fun v n:n -> 
+  method set =
+    fun value:v value_string:vs -> 
     if value <> v then begin
       value <- v; 
-      value_string <- n;
+      value_string <- vs;
       setfun v
     end
 end
 
 
 type property =
-  | Bool of bool rval
   | Int of int rval
   | Float of float rval
   | String of string rval
+  | Bool of bool rval
   | Shadow of Gtk.Tags.shadow_type rval
   | Policy of Gtk.Tags.policy_type rval
+
+let bool_values = ["true", true; "false", false]
+
+let shadow_type_values = ["NONE", `NONE; "IN", `IN; "OUT", `OUT;
+		  "ETCHED_IN", `ETCHED_IN; "ETCHED_OUT", `ETCHED_OUT ]
+let policy_type_values = ["ALWAYS", `ALWAYS; "AUTOMATIC", `AUTOMATIC ]
+
+(* values is the string representation of the value *)
+let change_value_in_prop :value_string = function
+  | Int rval  -> rval#set value:(int_of_string value_string) :value_string
+  | Float rval ->  rval#set value:(float_of_string value_string) :value_string
+  | String rval -> rval#set value:value_string :value_string
+  | Bool rval ->  rval#set :value_string
+	value:(List.assoc value_string in:bool_values)
+  | Shadow rval -> rval#set :value_string
+	value:(List.assoc value_string in:shadow_type_values)
+  | Policy rval -> rval#set :value_string
+	value:(List.assoc value_string in:policy_type_values)
+
+let rec set_property_in_list name value_string = function
+  | (n, p) :: tl ->
+      if name = n then change_value_in_prop p :value_string
+      else  (set_property_in_list name value_string tl)
+  | [] -> failwith "set_property_in_list: property not found"
+
+
 
 
 (* these functions return a string except int and float *)
@@ -81,25 +108,20 @@ class prop_enumtype l :callback ?:packing :value = object(self)
   initializer
     self#entry#connect#changed callback:
       (fun _ ->
-	callback (List.assoc self#entry#text in:l) n:self#entry#text);
+	callback value:(List.assoc self#entry#text in:l) value_string:self#entry#text);
     self#entry#set_editable false;
     self#entry#set_text (List.assoc value in:revl)
 end
 
 
-class prop_bool = prop_enumtype ["true", true; "false", false]
-
-class prop_shadow =
-  prop_enumtype ["NONE", `NONE; "IN", `IN; "OUT", `OUT;
-		  "ETCHED_IN", `ETCHED_IN; "ETCHED_OUT", `ETCHED_OUT ]
-
-class prop_policy =
-  prop_enumtype ["ALWAYS", `ALWAYS; "AUTOMATIC", `AUTOMATIC ]
+class prop_bool = prop_enumtype bool_values
+class prop_shadow = prop_enumtype shadow_type_values
+class prop_policy = prop_enumtype policy_type_values
 
 class prop_string :callback ?:packing :value = object(self)
   inherit entry text:value ?:packing show:true
   initializer
-    self#connect#activate callback:(fun _ -> callback self#text n:"");
+    self#connect#activate callback:(fun _ -> callback value:self#text value_string:"");
     ()
 end
 
@@ -112,7 +134,7 @@ object(self)
       show:true ?:packing
   initializer
     self#connect#activate callback:
-      (fun _ -> callback self#value_as_int n:"");
+      (fun _ -> callback value:self#value_as_int value_string:"");
     ()
 end
 
@@ -125,13 +147,13 @@ object(self)
       show:true ?:packing
   initializer
     self#connect#activate callback:
-      (fun _ -> callback self#value n:"");
+      (fun _ -> callback value:self#value value_string:"");
     ()
 end
 
 let plist_affich list =
   let une_prop_affich prop = 
-    let hbox = new box `HORIZONTAL show:true homogeneous:true in
+    let hbox = new hbox homogeneous:true in
     begin match prop with
     | name, Bool prop -> new label text:name show:true
 	  packing:(hbox#pack fill:true);
@@ -168,7 +190,7 @@ let plist_affich list =
 	()
     end;
     hbox
-  in let vbox = new box `VERTICAL show:true in
+  in let vbox = new vbox in
   List.iter fun:(fun (n, p) -> vbox#pack (une_prop_affich (n,p));
 	new separator `HORIZONTAL show:true packing:vbox#pack; ()) list;
   vbox
@@ -177,9 +199,8 @@ let plist_affich list =
 
 
 let propwin = new window show:true title:"properties"
-let vbox = new box `VERTICAL show:true packing:propwin#add
-let hbox = new box `HORIZONTAL show:true
-    packing:(vbox#pack padding:5);;
+let vbox = new vbox packing:propwin#add
+let hbox = new hbox packing:(vbox#pack padding:5);;
 
 new separator `HORIZONTAL packing:vbox#pack show:true;
 new separator `HORIZONTAL packing:vbox#pack show:true;;
@@ -210,20 +231,29 @@ propwin#connect#event#focus_in callback:
   
 cb#entry#connect#changed callback:
     (fun _ ->
-      let vb = plist_affich (List.assoc cb#entry#text in:!rname_prop_list) in
-      vbox#remove !vboxref;
-      vbox#pack vb;
-      vboxref := vb;
-      if cb#entry#misc#has_focus then begin
-	match !last_sel with
-	| None -> ()
-	| Some sl -> sl#base#misc#set state:`NORMAL
-      end;
-      let n = cb#entry#text in
-      let w = List.find !rwidget_list pred:(fun w -> w#name = n) in
-      if cb#entry#misc#has_focus then w#base#misc#set state:`SELECTED;
-      last_sel := Some w
-	  );;
+      let text = cb#entry#text in
+(* text = "" when we remove the last window *)
+      if text <> "" then begin
+	let vb = plist_affich (List.assoc text in:!rname_prop_list) in
+	vbox#remove !vboxref;
+	vbox#pack vb;
+	vboxref := vb;
+	if cb#entry#misc#has_focus then begin
+	  match !last_sel with
+	  | None -> ()
+	  | Some sl -> sl#base#misc#set state:`NORMAL
+	end;
+	let n = cb#entry#text in
+	let w = List.find !rwidget_list pred:(fun w -> w#name = n) in
+	if cb#entry#misc#has_focus then w#base#misc#set state:`SELECTED;
+	last_sel := Some w
+      end
+      else begin
+	let vb = new vbox in
+	vbox#remove !vboxref;
+	vbox#pack vb;
+	vboxref := vb;
+      end  );;
 
 let property_add rw =
     rwidget_list := (rw :> rwidget_base) :: !rwidget_list;
@@ -232,18 +262,22 @@ let property_add rw =
     cb#set_combo popdown_strings:(List.map fun:fst nplist)
 
 let property_remove rw =
-  let rec aux = function
+  let rec list_remove = function
     |	[] -> []
-    |	hd :: tl -> if hd = (rw :> rwidget_base) then tl else hd :: (aux tl)
-  in rwidget_list := aux !rwidget_list;
+    |	hd :: tl -> if hd = (rw :> rwidget_base) then tl else hd :: (list_remove tl)
+  in rwidget_list := list_remove !rwidget_list;
+  if !rwidget_list <> [] then begin
+    let nplist = List.map !rwidget_list fun:(fun w -> (w#name, w#proplist)) in
+    rname_prop_list := nplist;
+    cb#set_combo popdown_strings:(List.map fun:fst nplist)
+  end
+  else cb#entry#set_text ""
+
+let property_update name =
   let nplist = List.map !rwidget_list fun:(fun w -> (w#name, w#proplist)) in
   rname_prop_list := nplist;
-  cb#set_combo popdown_strings:(List.map fun:fst nplist)
-
-let property_update () =
-  let nplist = List.map fun:(fun (w : rwidget_base) -> (w#name, w#proplist)) !rwidget_list in
-  rname_prop_list := nplist;
-  cb#set_combo popdown_strings:(List.map fun:fst nplist)
+  cb#set_combo popdown_strings:(List.map fun:fst nplist);
+  cb#entry#set_text name
 
 let test_unique name = not (List.mem_assoc name in:!rname_prop_list)
 
