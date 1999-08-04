b99ca15952c2d185e3daa72d800fcbf469e733ff
kagawa <kagawa@9e25d42d-7a03-43f2-900d-8c2168964d28>
1999-08-04 09:51:53 +0000

    Aug 4th, kagawa

--- applications/lablglade/examples/main.ml
+++ applications/lablglade/examples/main.ml
@@ -16,10 +16,28 @@ let help_menu = factory#add_submenu label:"Help"
 
 let projwin = new GProjectWindow.project_window packing: vbox#add
 
+let cb_hash = Hashtbl.create size:57
+
+let edit = LglEditor.sig_edit
+let editcb () = let oldStr = edit#get_name () in 
+                let newStr = GladeProperty.Property.get_combo () in
+                if (newStr != "gtk_widget_destroy" && newStr <> oldStr) then
+                begin
+                  let txt = edit#get_text () in
+                  Hashtbl.remove cb_hash key:oldStr;
+                  Hashtbl.add cb_hash key:oldStr data:txt;
+                  edit#set_name newStr;
+                  let newTxt = try
+                                  Hashtbl.find cb_hash key:newStr
+                               with Not_found -> newStr ^ " = \n" in
+                  edit#load_text newTxt
+                end
+
 let main () =
   window#connect#event#delete 
     callback:(fun _ -> prerr_endline "Delete event occured"; Main.quit (); true);
   window#connect#destroy callback:Main.quit;
+  
   let factory = new GMenu.factory file_menu :accel_group in
   factory#add_item label:"New" callback:projwin#new_project;
   factory#add_item label:"Open..." key:'O' callback:projwin#on_open_project;
@@ -30,11 +48,15 @@ let main () =
   factory#add_item label:"Project Options..." callback:projwin#edit_options;
   factory#add_separator ();
   factory#add_item label:"Quit" key:'Q' callback:window#destroy;
+  
   let factory = new GMenu.factory edit_menu :accel_group in
   factory#add_item label:"Cut" key:'X' callback:projwin#cut;
   factory#add_item label:"Copy" key:'C' callback:projwin#copy;
   factory#add_item label:"Paste" key:'V' callback:projwin#paste;
   factory#add_item label:"Delete" callback:projwin#delete;
+  factory#add_separator ();
+  factory#add_item label:"Edit Callbacks" callback:editcb;
+  
   let factory = new GMenu.factory view_menu :accel_group in
   factory#add_item label:"Show Palette" callback:projwin#show_palette;
   factory#add_item label:"Show Properties" callback:projwin#show_property_editor;
@@ -43,6 +65,7 @@ let main () =
   factory#add_check_item label:"Show Widget Tooltips" active:true
     callback:(fun _ -> projwin#toggle_tooltips ());
   let grid_menu = factory#add_submenu label:"Grid" in
+  
   let factory = new GMenu.factory grid_menu :accel_group in
   factory#add_check_item label:"Show Grid" active:true
     callback:(fun _ -> projwin#toggle_grid ());
@@ -50,18 +73,20 @@ let main () =
   factory#add_check_item label:"Show Snap to Grid" active:true
     callback:(fun _ -> projwin#toggle_snap ());
   factory#add_item label:"Set Snap Options..." callback:projwin#edit_snap_settings;
+
   let factory = new GMenu.factory help_menu :accel_group in
   factory#add_item label:"About..." callback:projwin#about;
   window#add_accel_group accel_group;
   projwin#show_palette ();
   projwin#show_property_editor ();
   projwin#show_widget_tree ();
-  projwin#show_clipboard ();
+(*  projwin#show_clipboard (); *)
   let proj = GladeProject.Project.create() in
 (*  GladeProject.Project.set_source_files proj "gladesrc.ml" "gladesrc.mli"
                                              "gladesig.ml" "gladesig.mli"; *)
   projwin#set_project proj;
   window#show ();
+  edit#show ();
   Main.main ()
 
 let _ = Printexc.print main ()
