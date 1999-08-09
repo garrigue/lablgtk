c219ec9fd15514f82b8f4ba67905e569af304d8c
(no author) <(no author)@9e25d42d-7a03-43f2-900d-8c2168964d28>
1999-08-09 02:03:16 +0000

    This commit was manufactured by cvs2svn to create tag 'changing_set'.
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/tags/changing_set@250 9e25d42d-7a03-43f2-900d-8c2168964d28

--- applications/lablglade/glade.ml
+++
@@ -1,8 +0,0 @@
-type project = [project]
-type project_view = [widget container clist project_view]
-type project_window = [widget container box project_window]
-
-type gb_accelerator
-type gb_signal
-type gb_widget_data
-type gb_style
