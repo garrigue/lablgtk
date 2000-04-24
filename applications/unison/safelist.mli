68747cd9085d566a886f58904720cb08759e2705
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2000-04-24 09:13:46 +0000

    merge in ocaml 3.00 changes

--- applications/unison/safelist.mli
+++ applications/unison/safelist.mli
@@ -1,5 +1,5 @@
 (* $I1: Unison file synchronizer: src/safelist.mli $ *)
-(* $I2: Last modified by bcpierce on Mon, 24 Jan 2000 17:48:01 -0500 $ *)
+(* $I2: Last modified by vouillon on Wed, 16 Feb 2000 15:30:55 -0500 $ *)
 (* $I3: Copyright 1999-2000 (see COPYING for details) $ *)
 
 (* All functions here are tail recursive and will work for arbitrary
@@ -7,31 +7,34 @@
    the built-in List module should not be referred to outside this module. *)
 
 (* Functions from built-in List module *)
-val map : fun:('a -> 'b) -> 'a list -> 'b list
+val map : f:('a -> 'b) -> 'a list -> 'b list
 val append : 'a list -> 'a list -> 'a list
+val rev_append : 'a list -> 'a list -> 'a list
 val concat : 'a list list -> 'a list       
 val combine : 'a list -> 'b list -> ('a * 'b) list 
-val iter : fun:('a -> unit) -> 'a list -> unit
+val iter : f:('a -> unit) -> 'a list -> unit
 val rev : 'a list -> 'a list
-val fold_right : fun:('a -> acc:'b -> 'b) -> 'a list -> acc:'b -> 'b
+val fold_right : f:('a -> 'b -> 'b) -> 'a list -> init:'b -> 'b
 val hd : 'a list -> 'a
 val tl : 'a list -> 'a list
-val nth : 'a list -> pos:int -> 'a
+val nth : 'a list -> int -> 'a
 val length : 'a list -> int
-val mem : item:'a -> 'a list -> bool
+val mem : 'a -> 'a list -> bool
 val flatten : 'a list list -> 'a list
-val assoc : key:'a -> ('a * 'b) list -> 'b
-val for_all : pred:('a -> bool) -> 'a list -> bool
-val exists : pred:('a -> bool) -> 'a list -> bool
+val assoc : 'a -> ('a * 'b) list -> 'b
+val for_all : f:('a -> bool) -> 'a list -> bool
+val exists : f:('a -> bool) -> 'a list -> bool
 val split : ('a * 'b) list -> 'a list * 'b list
-val filter : pred:('a -> bool) -> 'a list -> 'a list
-val remove_assoc : key:'a -> ('a * 'b) list -> ('a * 'b) list
-val fold_left: fun:(acc:'a -> 'b -> 'a) -> acc:'a -> 'b list -> 'a
-val map2 : fun:('a -> 'b -> 'c) -> 'a list -> 'b list -> 'c list
+val filter : f:('a -> bool) -> 'a list -> 'a list
+val remove_assoc : 'a -> ('a * 'b) list -> ('a * 'b) list
+val fold_left: f:('a -> 'b -> 'a) -> init:'a -> 'b list -> 'a
+val map2 : f:('a -> 'b -> 'c) -> 'a list -> 'b list -> 'c list
+val iter2 : f:('a -> 'b -> unit) -> 'a list -> 'b list -> unit
 
 (* Other useful list-processing functions *)
-val filterMap : fun:('a -> 'b option) -> 'a list -> 'b list
-val splitN : 'a list list -> pos:int -> 'a list list
-val filterBoth : pred:('a -> bool) -> 'a list -> ('a list * 'a list)
+val filterMap : f:('a -> 'b option) -> 'a list -> 'b list
+val filterMap2 :
+    f:('a -> 'b option * 'c option) -> 'a list -> 'b list * 'c list
+val transpose : 'a list list -> 'a list list
+val filterBoth : f:('a -> bool) -> 'a list -> ('a list * 'a list)
 val allElementsEqual : 'a list -> bool
-
