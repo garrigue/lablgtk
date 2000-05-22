5866673771b3c2fe9902585a9818a6ef0aeb952e
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2000-05-22 09:56:15 +0000

    Changes for gtkMozilla

--- src/ml_gtk.h
+++ src/ml_gtk.h
@@ -1,9 +1,12 @@
 /* $Id$ */
 
 #define GtkObject_val(obj) ((GtkObject*)Field(obj,1))
-extern value Val_GtkObject (GtkObject *w);
+value Val_GtkObject (GtkObject *w);
+value Val_GtkObject_sink (GtkObject *w);
 #define Val_GtkAny(w) Val_GtkObject((GtkObject*)w)
 #define Val_GtkAny_sink(w) Val_GtkObject_sink((GtkObject*)w)
+#define Val_GtkWidget Val_GtkAny
+#define Val_GtkWidget_sink Val_GtkAny_sink
 
 #ifdef GTK_NO_CHECK_CASTS
 #define check_cast(f,v) f(Pointer_val(v))
