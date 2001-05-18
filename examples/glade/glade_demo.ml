65dc22d7dea5c2cc84cdbc73bc93d54332fe2d93
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2001-05-18 09:37:38 +0000

    update examples
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@527 9e25d42d-7a03-43f2-900d-8c2168964d28

--- examples/glade/glade_demo.ml
+++ examples/glade/glade_demo.ml
@@ -2,45 +2,32 @@
 
 (* An experiment on using libglade in lablgtk *)
 
-class window1 ~file =
-  let xml = Glade.create ~file ~root:"window1" in
+(* labgladecc project1.glade > project1.ml *)
+#use "project1.ml";;
+
+class editor () =
   object (self)
-    val window1 =
-      new GWindow.window (GtkWindow.Window.cast
-                            (Glade.get_widget xml ~name:"window1"))
-    val text1 =
-      new GEdit.text (GtkEdit.Text.cast
-                       (Glade.get_widget xml ~name:"text1"))
-    method xml = xml
+    inherit window1 ()
 
     method open_file () =
       let fs = GWindow.file_selection ~title:"Open file" ~modal:true () in
       fs#cancel_button#connect#clicked ~callback:fs#destroy;
       fs#ok_button#connect#clicked ~callback:
         begin fun () ->
-          text1#delete_text ~start:0 ~stop:text1#length;
+          self#text1#delete_text ~start:0 ~stop:self#text1#length;
           fs#destroy ()
         end;
       fs#show ()
+
     initializer
-      Glade.bind_handlers xml
-        ~extra:["on_open1_activate", `Simple self#open_file];
-      text1#set_editable true
+      self#bind ~name:"on_open1_activate" ~callback:self#open_file;
+      self#text1#set_editable true
   end
 
-(* This is to show bindings *)
-let ($) f g x = g (f x)
-let show_option sh = function None -> "None" | Some x -> "Some " ^ sh x
-let f ~handler ~signal ~after ?target obj =
-  Printf.printf "object=%s, signal=%s, handler=%s, target=%s\n"
-   (Glade.get_widget_name (GtkBase.Widget.cast obj)) signal handler
-   (show_option (GtkBase.Widget.cast $ Glade.get_widget_name) target)
-
 let main () =
-  Glade.init ();
-  let window1 = new window1 ~file:"project1.glade" in
+  let editor = new editor () in
   (* show bindings *)
-  Glade.signal_autoconnect window1#xml ~f; flush stdout;
+  Glade.print_bindings stdout editor#xml;
   GtkMain.Main.main ()
 
 let _ = main ()
