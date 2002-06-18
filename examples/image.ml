2647ed5d9591c0a154ea6ede21e38e1a2a035fba
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2002-06-18 10:46:38 +0000

    add Gdk.Rgb.draw_image

--- examples/image.ml
+++ examples/image.ml
@@ -4,22 +4,37 @@ open GMain
 open Gdk
 
 (* load image *)
-let buf = String.create (256*256*3)
-let ic = open_in_bin "image256x256.rgb"
-let _ = 
+let load_image file =
+  let buf = String.create (256*256*3) in
+  let ic = open_in_bin file in
   really_input ic buf 0 (256*256*3);
-  close_in ic
+  close_in ic;
+  buf
 
-let rgb_at x y =
+let rgb_at buf x y =
   let offset = (y * 256 + x) * 3 in
   (int_of_char buf.[offset  ],
    int_of_char buf.[offset+1],
    int_of_char buf.[offset+2])
 
-(* let id = Thread.create GtkThread.main () *)
+(* alternate approach: map the file *)
+(* Requires bigarray.cma, but needed for Rgb.draw_image *)
+(*
+let load_image file =
+  let fd = Unix.openfile file [Unix.O_RDONLY] 0 in
+  let arr =
+    Bigarray.Array1.map_file fd Bigarray.int8_unsigned Bigarray.c_layout
+      false (256*256*3) in
+  Unix.close fd;
+  arr
+
+let rgb_at buf x y =
+  let offset = (y * 256 + x) * 3 in
+  (buf.{offset}, buf.{offset+1}, buf.{offset+2})
+*)
 
 (* Choose a visual appropriate for RGB *)
-let _ =
+let () =
   Gdk.Rgb.init ();
   GtkBase.Widget.set_default_visual (Gdk.Rgb.get_visual ());
   GtkBase.Widget.set_default_colormap (Gdk.Rgb.get_cmap ())
@@ -34,34 +49,39 @@ let color_create = Truecolor.color_creator visual
 let w = window#misc#window
 let drawing = new GDraw.drawable w
 
-let _ =
+let () =
   window#connect#destroy ~callback:Main.quit;
 
+  (* Using images *)
   let image =
     Image.create ~kind: `FASTEST ~visual: visual ~width: 256 ~height: 256
   in
 
-  let draw () =
-    for x = 0 to 255 do
-      for y = 0 to 255 do
-        let r,g,b = rgb_at x y in
-        Image.put_pixel image ~x: x ~y: y 
-          ~pixel: (color_create ~red: (r * 256) ~green: (g * 256) ~blue: (b * 256))
-      done
-    done 
-  in
+  let buf = load_image "image256x256.rgb" in
+  for x = 0 to 255 do
+    for y = 0 to 255 do
+      let r,g,b = rgb_at buf x y in
+      Image.put_pixel image ~x: x ~y: y ~pixel:
+        (color_create ~red: (r * 256) ~green: (g * 256) ~blue: (b * 256))
+    done
+  done; 
  
-  let display () =
-    drawing#put_image image ~x:0 ~y:0
-  in
+  let display () = drawing#put_image image ~x:0 ~y:0 in
 
-  draw (); 
+  (* Using Rgb.draw_image *)
+  (*
+  let pixmap = Pixmap.create ~window:w ~width:256 ~height:256 () in
+  let buf = load_image "image256x256.rgb" in
+  Rgb.draw_image pixmap (GC.create pixmap) ~width:256 ~height:256 buf;
+    
+  let display () = drawing#put_pixmap pixmap ~x:0 ~y:0 in
+  *)
 
+  (* Bind display callback *)
   window#event#connect#after#expose ~callback:
     begin fun _ ->
       display (); false
     end;
-  (* Thread.join id *)
 
   window#show ();
   Main.main ()
