95d76ee14908f29198feac4c1d56edb487dcc28e
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2002-06-19 15:08:40 +0000

    interface
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@615 9e25d42d-7a03-43f2-900d-8c2168964d28

--- examples/image.ml
+++ examples/image.ml
@@ -5,6 +5,7 @@ open Gdk
 
 (* load image *)
 let load_image file =
+  print_endline "Load as string";
   let buf = String.create (256*256*3) in
   let ic = open_in_bin file in
   really_input ic buf 0 (256*256*3);
@@ -23,6 +24,7 @@ let create_region = Gpointer.region_of_string
 (* Requires bigarray.cma, but needed for Rgb.draw_image *)
 (*
 let load_image file =
+  print_endline "Map file to bigarray";
   let fd = Unix.openfile file [Unix.O_RDONLY] 0 in
   let arr =
     Bigarray.Array1.map_file fd Bigarray.int8_unsigned Bigarray.c_layout
@@ -36,6 +38,15 @@ let rgb_at buf x y =
 
 let create_region = Gpointer.region_of_bigarray
 *)
+
+let use_rgb = ref false
+let file = ref "image256x256.rgb"
+let () =
+  Arg.parse
+    ["-rgb", Arg.Set use_rgb, "  use Gdk.Rgb.draw_image";
+     "-image", Arg.Clear use_rgb, "use Gdk.Image.draw_image" ]
+    (fun f -> file := f)
+    (Sys.argv.(0) ^ " <256x256 rgb file> (edit for using Bigarray)")
 
 (* Choose a visual appropriate for RGB *)
 let () =
@@ -53,35 +64,29 @@ let color_create = Truecolor.color_creator visual
 let w = window#misc#window
 let drawing = new GDraw.drawable w
 
-let () =
-  window#connect#destroy ~callback:Main.quit;
+let display =
+  let buf = load_image !file in
 
-  (* Using images *)
-
-  let image =
-    Image.create ~kind: `FASTEST ~visual: visual ~width: 256 ~height: 256
-  in
-
-  let buf = load_image "image256x256.rgb" in
-  for x = 0 to 255 do
-    for y = 0 to 255 do
+  if not !use_rgb then begin
+    print_endline "Using Gdk.Image";
+    let image =
+      Image.create ~kind: `FASTEST ~visual: visual ~width: 256 ~height: 256 in
+    for x = 0 to 255 do for y = 0 to 255 do
       let r,g,b = rgb_at buf x y in
       Image.put_pixel image ~x: x ~y: y ~pixel:
         (color_create ~red: (r * 256) ~green: (g * 256) ~blue: (b * 256))
-    done
-  done; 
- 
-  let display () = drawing#put_image image ~x:0 ~y:0 in
+    done done; 
+    fun () -> drawing#put_image image ~x:0 ~y:0
+  end else begin
+    print_endline "Using Gdk.Rgb";
+    let reg = create_region buf in
+    fun () -> drawing#put_rgb_data reg ~width:256 ~height:256
+  end
 
-
-  (* Using Rgb.draw_image *)
-  (*
-  let buf = create_region (load_image "image256x256.rgb") in
-
-  let display () = drawing#put_rgb_data buf ~width:256 ~height:256 in
-   *)
-
-  (* Bind display callback *)
+let () =
+  flush stdout;
+  (* Bind callbacks *)
+  window#connect#destroy ~callback:Main.quit;
   window#event#connect#after#expose ~callback:
     begin fun _ ->
       display (); false
