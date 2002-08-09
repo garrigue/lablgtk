78a7c6a9fa941b2f0f4f26e7a111ada12783d5bf
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2002-08-09 02:46:29 +0000

    threads
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/trunk@650 9e25d42d-7a03-43f2-900d-8c2168964d28

--- src/lablgtk.bat
+++ src/lablgtk.bat
@@ -1,4 +1,9 @@
 @echo off
 rem start lablgtk
 
+if "%1" == "-thread" goto thread
 ocaml -w s -I +lablgtk lablgtk.cma gtkInit.cmo %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto done
+:thread
+ocaml -w s -I +threads unix.cma threads.cma -I +lablgtk lablgtk.cma gtkThread.cmo gtkInit.cmo gtkThInit.cmo %2 %3 %4 %5 %6 %7 %8 %9
+:done
