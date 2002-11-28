7de7a7dca6780a84f994987046d5c9f291d29691
monate <monate@9e25d42d-7a03-43f2-900d-8c2168964d28>
2002-11-28 16:47:50 +0000

    A simple utf8 viewer
    git-svn-id: svn+ssh://svn.gna.org/svn/lablgtk/branches/lablgtk2@694 9e25d42d-7a03-43f2-900d-8c2168964d28

---
+++ examples/text/unicode_viewer.ml
@@ -0,0 +1,35 @@
+
+let file_name = 
+  try
+    Sys.argv.(1)
+  with _ -> 
+    prerr_endline "Usage : unicode_viewer <file_name>"; 
+    exit 1 
+;;
+
+GtkMain.Main.init ();;
+
+let f_to_string n = 
+  let ic = open_in_bin n in 
+  let s = ref "" in
+    try while true do
+      s:= !s ^ (input_line ic) ^ "\n"
+    done;
+      !s
+    with End_of_file -> close_in ic ; !s
+     
+let main () = 
+  let w = GWindow.window 
+	    ~width:640 ~height:480 ~title:"2)view_with_buffer" ()
+  in
+  let sw = GBin.scrolled_window ~packing:(w#add) () in
+  let b = GText.buffer () in
+  let s = f_to_string file_name in
+    b#set_text ~text:s ();
+    let tv = GText.view ~buffer:b ~packing:(sw#add) () in
+      w#show ();;
+
+
+main () ;; 
+
+GMain.Main.main ();;
