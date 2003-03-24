b2917a43624c43df3156c8c78f36d0646191a2ec
oandrieu <oandrieu@9e25d42d-7a03-43f2-900d-8c2168964d28>
2003-03-24 23:28:20 +0000

    added stock items

--- src/ml_gtkstock.c
+++ src/ml_gtkstock.c
@@ -6,10 +6,12 @@
 #include <caml/fail.h>
 
 #include "wrappers.h"
+#include "ml_glib.h"
 #include "ml_gobject.h"
 #include "ml_gtk.h"
 #include "ml_gdkpixbuf.h"
 #include "gtk_tags.h"
+#include "gdk_tags.h"
 
 /* gtkiconfactory.h */
 
@@ -64,3 +66,23 @@ ML_2 (gtk_icon_factory_lookup, GtkIconFactory_val, String_val, Val_GtkIconSet)
 ML_1 (gtk_icon_factory_add_default, GtkIconFactory_val, Unit)
 ML_1 (gtk_icon_factory_remove_default, GtkIconFactory_val, Unit)
 ML_1 (gtk_icon_factory_lookup_default, String_val, Val_GtkIconSet)
+
+/* GtkStockItem */
+static Make_Flags_val (GdkModifier_val)
+
+CAMLprim value ml_gtk_stock_add(value item)
+{
+  GtkStockItem it;
+  it.stock_id = String_val(Field(item, 0));
+  it.label    = String_val(Field(item, 1));
+  it.modifier = Flags_GdkModifier_val(Field(item, 2)) ;
+  it.keyval   = Int_val(Field(item, 3));
+  it.translation_domain = NULL;
+  gtk_stock_add(&it, 1);
+  return Val_unit;
+}
+
+CAMLprim value ml_gtk_stock_list_ids(value unit)
+{
+  return Val_GSList_free( gtk_stock_list_ids(), (value_in) copy_string_g_free);
+}
