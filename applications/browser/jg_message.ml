385a36a8d09632b9518648b0e322bc4464c049c5
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2004-11-19 01:41:32 +0000

    for csl308

--- applications/browser/jg_message.ml
+++ applications/browser/jg_message.ml
@@ -69,7 +69,7 @@ let formatted ~title ?on ?(ppf = Format.std_formatter)
 
 let ask ~title ?master ?(no=true) ?(cancel=true) text =
   let tl = GWindow.dialog ~title ~modal:true () in
-  Gaux.may tl#set_transient_for master;
+  Gaux.may (fun w -> tl#set_transient_for w#as_window) master;
   GMisc.label ~text ~packing:tl#vbox#add ~xpad:20 ~ypad:10
     ~width:250 ~justify:`LEFT ~line_wrap:true ~xalign:0. ();
   let r = ref `Cancel in
