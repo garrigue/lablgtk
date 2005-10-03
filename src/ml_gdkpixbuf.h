c8ccd9e8fc9a9cfa36a3a31d10ef52ebd1560272
oandrieu <oandrieu@9e25d42d-7a03-43f2-900d-8c2168964d28>
2005-10-03 11:12:53 +0000

    * fix refcounting of pixbufs in GdkPixbuf

--- src/ml_gdkpixbuf.h
+++ src/ml_gdkpixbuf.h
@@ -2,5 +2,5 @@
 
 #define GdkPixbuf_val(val)       (check_cast(GDK_PIXBUF, val))
 value Val_GdkPixbuf_ (GdkPixbuf *, gboolean);
-#define Val_GdkPixbuf(p)         Val_GdkPixbuf_(p, FALSE)
-#define Val_GdkPixbuf_new(p)     Val_GdkPixbuf_(p, TRUE)
+#define Val_GdkPixbuf(p)         Val_GdkPixbuf_(p, TRUE)
+#define Val_GdkPixbuf_new(p)     Val_GdkPixbuf_(p, FALSE)
