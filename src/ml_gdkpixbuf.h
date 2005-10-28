6a7596d88a8605a0cf1cffa1594e4418917dd88e
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2005-10-28 02:01:28 +0000

    try compiling lablrsvg on windows

--- src/ml_gdkpixbuf.h
+++ src/ml_gdkpixbuf.h
@@ -1,6 +1,6 @@
 /* $Id$ */
 
 #define GdkPixbuf_val(val)       (check_cast(GDK_PIXBUF, val))
-value Val_GdkPixbuf_ (GdkPixbuf *, gboolean);
+CAMLexport value Val_GdkPixbuf_ (GdkPixbuf *, gboolean);
 #define Val_GdkPixbuf(p)         Val_GdkPixbuf_(p, TRUE)
 #define Val_GdkPixbuf_new(p)     Val_GdkPixbuf_(p, FALSE)
