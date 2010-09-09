771025dc8b64cc85a0d2731f3d1e149c70116acc
garrigue <garrigue@9e25d42d-7a03-43f2-900d-8c2168964d28>
2010-09-09 07:15:36 +0000

    fix bug in ml_gtk_curve_get_vector

--- examples/curve.ml
+++ examples/curve.ml
@@ -1,13 +1,15 @@
 (* $Id$ *)
 
 let w = GWindow.window ~width:200 ~height:150 ();;
-let curve = GMisc.curve ~packing:w#add ();;
+let curve = GMisc.curve ~min_y:0. ~max_y:10. ~packing:w#add ();;
+let show_vector _ =
+  let vect = curve#get_vector 5 in
+  Printf.printf "%g %g %g %g %g\n%!"
+    vect.(0) vect.(1) vect.(2) vect.(3) vect.(4)
 let () =
-  curve#event#connect#after_any ~callback:
-    (fun _ ->
-      let vect = curve#get_vector 5 in
-      Printf.printf "%g %g %g %g %g\n%!"
-        vect.(0) vect.(1) vect.(2) vect.(3) vect.(4));
+  curve#set_vector [|0.; 5.; 4.; 6.|];
+  show_vector ();
+  curve#event#connect#after_any ~callback:show_vector;
   w#connect#destroy ~callback:GMain.quit;
   w#show ();
   GMain.main ()
