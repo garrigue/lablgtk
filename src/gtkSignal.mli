d46e91491940e0ab490e89252889b795442ef947
Adrien Nader <camaradetux@gmail.com>
2011-03-10 10:44:48 +0100

    g_signal_new: create the ocaml type.

--- src/gtkSignal.mli
+++ src/gtkSignal.mli
@@ -51,7 +51,7 @@ val safe_call : ?where:string -> ('a -> unit) -> 'a -> unit
     (** Safe wrapper for function calls. Tries to handle exceptions
         with user_handler, and reports an error otherwise. *)
 
-external signal_new : unit -> unit = "ml_g_signal_new_me"
+external signal_new : Gobject.signal_type -> unit = "ml_g_signal_new_me"
 
 external connect_by_name :
   'a obj -> name:string -> callback:g_closure -> after:bool -> id
