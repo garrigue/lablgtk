8c5d676ba5caf46e6910e2dfa859d8fc0bda6dcc
Adrien Nader <adrien@notk.org>
2012-06-20 22:36:17 +0200

    GToolbox: keyboard shortcuts: change shortcut_specification type.

--- src/gToolbox.mli
+++ src/gToolbox.mli
@@ -149,23 +149,26 @@ val tree_selection_dialog :
   ?width:int -> ?height:int ->
   ?show:bool -> unit -> 'a option
 
-(** {2 Keyboard shortcuts} *)
+(** {2 Keyboard shortcuts}
+Associate messages to key combinations. *)
 (** A keyboard shorcut: a combination of Alt, Control and Shift and a letter. *)
 type key_combination = [ `A | `C | `S ] list * char
 
 (** A shortcut specification: name of a GTK+ signal to emit, keyboard shortcuts
-   and the corresponding callback which has to be unit -> unit (at least for
-   now). *)
-type shortcut_specification = {
+   and the message to send. The name must be unique. *)
+type 'a shortcut_specification = {
   name : string;
   keys : key_combination list;
-  callback : unit -> unit;
+  message : 'a;
 }
 
 (** Setup the given shortcut spec list for the given window and callback.
- * This create the GTK+ signal, associate the keyboard shortcuts to it, make the
- * window listen to these shortcuts and eventually call them. *)
-val create_shortcuts : #GWindow.window_skel -> shortcut_specification list ->
+   This create the GTK+ signal, associate the keyboard shortcuts to it, make the
+   window listen to these shortcuts and eventually call the given callback with
+   the messages from the shortcut specification. *)
+val create_shortcuts : window:#GWindow.window_skel ->
+  shortcuts:'a shortcut_specification list ->
+  callback:('a -> unit) ->
   unit
 
 
