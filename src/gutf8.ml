50fdf1b09030928e871528dac8e5205895bdbb60
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2018-11-30 08:30:37 +0900

    make 4.05 a requirement, and ensure we compile on it

--- src/gutf8.ml
+++ src/gutf8.ml
@@ -65,17 +65,19 @@ let write_unichar s ~pos (c : unichar) =
   end;
   pos := p + len
 
+let sub_string s ~pos ~len = Bytes.sub_string s pos len
+
 let from_unichar (n : unichar) =
   let s = Bytes.create 6 and pos = ref 0 in
   write_unichar s ~pos n;
-  Bytes.sub_string s ~pos:0 ~len:!pos
+  sub_string s ~pos:0 ~len:!pos
 
 let from_unistring (s : unistring) =
   let len = Array.length s in
   let r = Bytes.create (len*6) in
   let pos = ref 0 in
   for i = 0 to len-1 do write_unichar r ~pos s.(i) done;
-  Bytes.sub_string r ~pos:0 ~len:!pos
+  sub_string r ~pos:0 ~len:!pos
 
 let rec hi_bits n =
   if n land 0x80 = 0 then 0 else
