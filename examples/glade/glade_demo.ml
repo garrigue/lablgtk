4a835eff8cba0d4df57d2593223e8a9cdfa5d1cc
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2018-12-21 18:31:11 +0100

    README and examples/glade ported to lablgladecc3
    Note: I did not bind the automatic management of signals
    that are specified in the XML file produced by Glade. I could,
    but since the callbacks can only be C code, it is not very useful.
    The examples, however, used to automatically connect the quit
    button to gtk_main_quit. I have preferred to add the code to the
    OCaml side and call GMain.quit instead, which seems safer anyway.

--- examples/glade/glade_demo.ml
+++ examples/glade/glade_demo.ml
@@ -10,34 +10,57 @@
 
 (* An experiment on using libglade in lablgtk *)
 
-(* lablgladecc2 project1.glade > project1.ml *)
+(* lablgladecc3 project1.ui > project1.ml *)
 #use "project1.ml";;
+
+let load_file f =
+  let ic = open_in f in
+  let n = in_channel_length ic in
+  let s = Bytes.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  Bytes.to_string s
 
 class editor () =
   object (self)
     inherit window1 ()
 
     method open_file () =
-      let fs = GWindow.file_selection ~title:"Open file" ~modal:true () in
-      fs#cancel_button#connect#clicked ~callback:fs#destroy;
-      fs#ok_button#connect#clicked ~callback:
-        begin fun () ->
-          self#textview1#buffer#set_text "";
-          fs#destroy ()
-        end;
-      fs#show ()
+     let fileSel = GWindow.file_chooser_dialog
+       ~action:`OPEN
+       ~title:"Open file"
+       ~modal:true
+       ~type_hint:`DIALOG
+       ~position:`CENTER
+       () in
+     fileSel#add_select_button_stock `OPEN `OK;
+     fileSel#add_button_stock `CANCEL `CANCEL;
+     (match fileSel#run () with
+      | `OK ->
+          (match fileSel#filename with
+              Some fn -> self#textview1#buffer#set_text (load_file fn)
+            | None -> ())
+      | `CANCEL -> ()
+      | `DELETE_EVENT -> ()) ;
+     fileSel#destroy ()
 
     initializer
-      self#bind ~name:"on_open1_activate" ~callback:self#open_file;
-      self#bind ~name:"on_about1_activate" 
-	~callback:
-	(fun () -> prerr_endline "XXX")
+      self#textview1#buffer#set_text "A text editor skeleton. Only File/Open and Help/About are implemented." ;
+      self#open1#connect#activate ~callback:self#open_file;
+      self#about1#connect#activate ~callback:(fun () ->
+       let d =
+        GWindow.about_dialog
+         ~name:"Editor skeleton"
+         ~authors:["Anonymous coward"]
+         () in
+       d#set_logo (GMisc.image ~file:"logo.jpg" ())#pixbuf ;
+       d#show ());
+      ()
   end
 
 let main () =
   let editor = new editor () in
   (* show bindings *)
-  Glade.print_bindings stdout editor#xml;
   editor#window1#connect#destroy ~callback:GMain.quit;
   GMain.main ()
 
