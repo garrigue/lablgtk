754d0cc4bc32a24e01bf98d5f5fee211dc465a03
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-02-10 03:28:11 +0100

    [build] Detect gtk+-quartz better in dune_config
    This removes the need of an special flags [thus will work better on
    OSX]
    
    We also add Travis OSX support that may prove more convenient for us
    than Azure.

--- src/dune_config.ml
+++ src/dune_config.ml
@@ -10,20 +10,32 @@ module Option = struct
     | None   -> error message
     | Some v -> v
 
-  let cata f d = function
+  let cata ~f d = function
     | None -> d
     | Some x -> f x
 end
+
+(* This is a hack to detect gtk+-quartz *)
+let platform_subst p ~package =
+  match package with
+  | "gtk+-3.0" ->
+    Option.cata
+      ~f:(fun _ -> "gtk+-quartz-3.0", ["-DHAS_GTKQUARTZ"])
+      (package,[])
+      C.Pkg_config.(query p ~package:"gtk+-quartz-3.0")
+  | _ ->
+    package, []
 
 let query_pkg p ~package =
   Option.require ~message:(package ^ " not found") C.Pkg_config.(query p ~package)
 
 let gen_pkg p ~package ~version =
   let file kind = kind ^ "-" ^ package ^ ".sexp" in
+  let package, extra_flags = platform_subst p ~package in
   let package =
-    Option.cata (fun version -> Format.sprintf "%s >= %s" package version) package version in
+    Option.cata ~f:(fun version -> Format.sprintf "%s >= %s" package version) package version in
   let c_g = query_pkg p ~package in
-  C.Flags.write_sexp (file "cflag") c_g.C.Pkg_config.cflags;
+  C.Flags.write_sexp (file "cflag") @@ c_g.C.Pkg_config.cflags @ extra_flags;
   C.Flags.write_sexp (file "clink") c_g.C.Pkg_config.libs
 
 let pkg = ref ""
