7d259bac07d565b7bad7a37b5fd8f62c5b97de05
Hugo Ros <hugo.ros@protonmail.com>
2021-03-09 23:52:05 +0200

    Forgot some GtkStack transitions

--- examples/stackswitcher.ml
+++ examples/stackswitcher.ml
@@ -20,11 +20,13 @@ let main () =
   let switcher = GPack.stack_switcher () in
   let stack = GPack.stack ~transition_type:`SLIDE_LEFT_RIGHT () in
   switcher#set_stack stack#as_stack;
+  let transition_choice_button = GButton.check_button ~label:"Change transition type" () in
 
+  root_layout#pack ~expand:false ~fill:false transition_choice_button#coerce;
   root_layout#pack ~expand:false ~fill:false switcher#coerce;
   root_layout#pack ~expand:true ~fill:true stack#coerce;
   switcher#set_halign `CENTER;
-  
+
   let checkbox = GButton.check_button ~label:"Item"
     ~packing:(fun w -> stack#add_titled w "frame1" "Check button") () in
 
@@ -34,6 +36,11 @@ let main () =
   let entry = GEdit.entry ~placeholder_text:"Type here"
     ~packing:(fun w -> stack#add_titled w "frame3" "Form") () in
   
+  ignore (transition_choice_button#connect#toggled ~callback:(fun () ->
+    match transition_choice_button#active with
+    | true -> stack#set_transition_type `OVER_UP_DOWN
+    | false -> stack#set_transition_type `SLIDE_LEFT_RIGHT));
+
   window#show ();
   GMain.main ()
 
