a120daf5dd77b870d6919538594749b1deda538b
Hugo Ros <hugo.ros@protonmail.com>
2021-03-09 23:06:53 +0200

    Complete stack switcher example

--- examples/stackswitcher.ml
+++ examples/stackswitcher.ml
@@ -15,12 +15,25 @@ let main () =
     ~width:300 ~height:200 () in
   ignore (window#connect#destroy ~callback:GMain.quit);
 
-  let root_layout = GPack.vbox ~spacing:10 ~packing:window#add () in
+  let root_layout = GPack.box `VERTICAL ~spacing:10 ~homogeneous:false ~packing:window#add () in
   
-  let switcher = GPack.stack_switcher ~packing:root_layout#add () in
-  let stack = GPack.stack ~packing:root_layout#add () in
-  (* switcher#set_stack stack; *)
+  let switcher = GPack.stack_switcher () in
+  let stack = GPack.stack ~transition_type:`SLIDE_LEFT_RIGHT () in
+  switcher#set_stack stack#as_stack;
 
+  root_layout#pack ~expand:false ~fill:false switcher#coerce;
+  root_layout#pack ~expand:true ~fill:true stack#coerce;
+  switcher#set_halign `CENTER;
+  
+  let checkbox = GButton.check_button ~label:"Item"
+    ~packing:(fun w -> stack#add_titled w "frame1" "Check button") () in
+
+  let label = GMisc.label ~text:"A label"
+    ~packing:(fun w -> stack#add_titled w "frame2" "Label") () in
+
+  let entry = GEdit.entry ~placeholder_text:"Type here"
+    ~packing:(fun w -> stack#add_titled w "frame3" "Form") () in
+  
   window#show ();
   GMain.main ()
 
