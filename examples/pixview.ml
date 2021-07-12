7adb921b263df0044cc840116944e340a27470f0
Maxence Guesdon <maxence.guesdon@inria.fr>
2021-07-12 13:58:46 +0200

    resurrect support for librsvg2

--- examples/pixview.ml
+++ examples/pixview.ml
@@ -10,32 +10,31 @@
 
 (* An image viewer, supporting all formats allowed by GdkPixbuf *)
 
-let pb =
-  if Array.length Sys.argv < 2 then begin
+if Array.length Sys.argv < 2 then begin
     Printf.eprintf "usage : %s <file>\n" Sys.argv.(0);
-    exit 2;
-  end;
-  try GdkPixbuf.from_file Sys.argv.(1)
+    exit 2
+  end;;
+
+let file = Sys.argv.(1)
+let pb =
+  try
+    match String.lowercase_ascii (Filename.extension file) with
+    | "svg" -> Rsvg2.render_from_file file
+    | _ -> GdkPixbuf.from_file file
   with GdkPixbuf.GdkPixbufError(_,msg) as exn ->
     let d = GWindow.message_dialog ~message:msg ~message_type:`ERROR
         ~buttons:GWindow.Buttons.close ~show:true () in
     d#run ();
     raise exn
 
-let pm, _ = GdkPixbuf.create_pixmap pb
-
-let width = GdkPixbuf.get_width pb
-let height = GdkPixbuf.get_height pb
-
-let w = GWindow.window ~width ~height ~title:Sys.argv.(1) ()
-let da = GMisc.drawing_area ~packing:w#add ()
-
-let dw = da#misc#realize (); new GDraw.drawable da#misc#window
-
 let () =
   GMain.init ();
-  da#event#connect#expose (fun _ -> dw#put_pixmap ~x:0 ~y:0 pm; true);
+  let width = GdkPixbuf.get_width pb in
+  let height = GdkPixbuf.get_height pb in
+  let w = GWindow.window ~width ~height ~title:file () in
   w#connect#destroy GMain.quit;
+  let image = GMisc.image ~pixbuf:pb () in
+  w#add image#coerce ;
   w#show ();
   GMain.main ()
 
